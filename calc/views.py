d526f7f65d3aee5757851d9409df869120d2a202
farazrao <farazrajpoot15@gmail.com>
2020-04-16 15:09:58 +0200

    Add three files

--- calc/views.py
+++ calc/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request,'page.html',{'name':'faraz'})
--- shakeel/settings.py
+++ shakeel/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'shakeel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
